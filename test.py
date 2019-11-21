b195dda41960bd6eea11b1793436a27689e9cd00
vhschs <786605549@qq.com>
2019-11-21 16:49:51 +0800

    2019/11/21

--- test.py
+++ test.py
@@ -1,22 +1,33 @@
 from models.gdal_convert import ENVI_RAW
 from models.cv_circles_linesP import recongize_circle,recongize_linesP
 from matplotlib import pyplot as plt
+from PIL import Image, ImageTk
+
 import tkinter as tk
 
-# file = 'D:\\Desktop\\data\\BSQ\\306.raw.BSQ'
-file = 'Z:\\data\\BSQ\\306_ref.raw.BSQ'
+root = tk.Tk()
 
+pil_image = Image.fromarray(rgb)
+w, h = pil_image.size
+fname = 'None'
+# split off image file name
+sf = "{} ({}x{})".format(fname, w, h)
+root.title(sf)
+
+# convert PIL image object to Tkinter PhotoImage object
+tk_image = ImageTk.PhotoImage(pil_image)
+
+# put the image on a typical widget
+label = tk.Label(root, image=tk_image, bg='brown')
+label.pack(padx=5, pady=5)
+
+root.mainloop()
+
+
+# file = 'D:\\Desktop\\data\\BSQ\\306.raw.BSQ'
+# file = 'Z:\\data\\BSQ\\306_ref.raw.BSQ'
 raw = ENVI_RAW(file)
 circle = recongize_circle(raw.get_gray())
 print(circle.type)
 circle.train_search_output()
 rgb = circle.draw_all(raw.get_rgb())
-
-# plt initialize
-plt.figure('hyperspectral image', figsize=(19.2, 10.8), dpi=100)
-plt.suptitle('Hyperspectral Image', fontsize=40)
-plt.imshow(rgb)
-plt.title('rgb', verticalalignment='baseline',
-          horizontalalignment='center', fontsize=10)
-plt.xticks([]), plt.yticks([])
-plt.show()
